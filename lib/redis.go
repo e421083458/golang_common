2838e40598ea53ffd9c0ad113a0385611ce4f17f
niuyufu <niuyufu@didichuxing.com>
2019-08-11 20:15:28 +0800

    优化RedisConnFactory空指针问题

--- lib/redis.go
+++ lib/redis.go
@@ -9,15 +9,17 @@ import (
 )
 
 func RedisConnFactory(name string) (redis.Conn, error) {
-	for confName, cfg := range ConfRedisMap.List {
-		if name == confName {
-			randHost := cfg.ProxyList[rand.Intn(len(cfg.ProxyList))]
-			return redis.Dial(
-				"tcp",
-				randHost,
-				redis.DialConnectTimeout(50*time.Millisecond),
-				redis.DialReadTimeout(100*time.Millisecond),
-				redis.DialWriteTimeout(100*time.Millisecond))
+	if ConfRedisMap!=nil  && ConfRedisMap.List!=nil{
+		for confName, cfg := range ConfRedisMap.List {
+			if name == confName {
+				randHost := cfg.ProxyList[rand.Intn(len(cfg.ProxyList))]
+				return redis.Dial(
+					"tcp",
+					randHost,
+					redis.DialConnectTimeout(50*time.Millisecond),
+					redis.DialReadTimeout(100*time.Millisecond),
+					redis.DialWriteTimeout(100*time.Millisecond))
+			}
 		}
 	}
 	return nil, errors.New("create redis conn fail")
