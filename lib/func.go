0637b89996bc8d34fd00321765247be228e5dd32
niuyufu <niuyufu@didichuxing.com>
2020-03-18 18:09:09 +0800

    优化默认环境参数

--- lib/func.go
+++ lib/func.go
@@ -34,18 +34,13 @@ func Init(configPath string) error {
 
 //模块初始化
 func InitModule(configPath string,modules []string) error {
-	var conf *string
-	if len(configPath) > 0 {
-		conf = &configPath
-	} else {
-		conf = flag.String("config", "", "input config file like ./conf/dev/")
-		flag.Parse()
-	}
+	conf := flag.String("config", configPath, "input config file like ./conf/dev/")
+	flag.Parse()
 	if *conf == "" {
 		flag.Usage()
 		os.Exit(1)
 	}
-
+	
 	log.Println("------------------------------------------------------------------------")
 	log.Printf("[INFO]  config=%s\n", *conf)
 	log.Printf("[INFO] %s\n", " start loading resources.")
